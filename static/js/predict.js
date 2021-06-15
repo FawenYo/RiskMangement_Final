bb65c5aa966d18562b078003be586acd4eaed9a4
FawenYo <twsky020600@gmail.com>
2021-06-15 09:35:50 +0800

    Add more prediction server

--- static/js/predict.js
+++ static/js/predict.js
@@ -3,13 +3,27 @@ var paragraph = "";
 var question = "";
 
 $(document).ready(function() {
+    // Get server IP every 5 seconds
+    window.setInterval((() => {
+        getServer();
+    }), 5000)
+
     $("#predictBtn").click(function() {
         $(this).prop('disabled', true);
         $(this).css("display", "none");
         $("#loading_anim").css("display", "inline-block");
         paragraph = $("#paragraph").val()
         question = $("#question").val()
-        getServer();
+        predictGPU();
+    });
+
+    $("#predictBtn2").click(function() {
+        $(this).prop('disabled', true);
+        $(this).css("display", "none");
+        $("#loading_anim2").css("display", "inline-block");
+        paragraph = $("#paragraph").val()
+        question = $("#question").val()
+        predictHF();
     });
 });
 
@@ -21,23 +35,12 @@ function getServer() {
         method: "GET",
         success: function(data) {
             DOMAIN = data.server_url
-            predict();
         },
-        error: function() {
-            Swal.fire({
-                icon: 'error',
-                title: '很抱歉！',
-                text: '無法取得伺服器位置，請稍後再試！',
-                showCancelButton: false,
-                confirmButtonText: '確定',
-            })
-            init();
-        }
     });
 }
 
-
-function predict() {
+function predictGPU() {
+    let start = new Date();
 
     let sendData = {
         "QUESTION_TEXT": question,
@@ -57,8 +60,9 @@ function predict() {
     fetch(URL, requestOptions)
         .then(response => response.json())
         .then((data) => {
-            show_text = `<font color="red">預測結果：</font>${data.answer}<br><font color="grey">耗時：${data.execution}秒</font>`
-            init()
+            let end = new Date();
+            show_text = `<font color="red">預測結果：</font>${data.answer}<br><font color="grey">耗時：${(end-start)/1000}秒</font>`
+            init(true)
             Swal.fire({
                 icon: 'success',
                 title: '預測結果',
@@ -88,7 +92,7 @@ function predict() {
                         .then(response => response.json())
                         .then((data) => {
                             show_text = `<font color="red">預測結果：</font>${data.answer}<br><font color="grey">耗時：${data.execution}秒</font>`
-                            init()
+                            init(true)
                             Swal.fire({
                                 icon: 'success',
                                 title: '預測結果',
@@ -106,7 +110,7 @@ function predict() {
                                 showCancelButton: false,
                                 confirmButtonText: '確定',
                             })
-                            init();
+                            init(true);
                         });
                 },
             })
@@ -116,16 +120,60 @@ function predict() {
             Swal.fire({
                 icon: 'error',
                 title: '很抱歉！',
-                text: '發生錯誤，請稍後再試！',
+                text: '伺服器目前為離線狀態！',
                 showCancelButton: false,
                 confirmButtonText: '確定',
             })
-            init();
+            init(true);
         });
 }
 
-function init() {
-    $("#predictBtn").prop('disabled', false);
-    $("#predictBtn").css("display", "inline");
-    $("#loading_anim").css("display", "none");
+function predictHF() {
+    let start = new Date();
+    let sendData = { "inputs": { "question": question, "context": paragraph } };
+
+    const requestOptions = {
+        method: 'POST',
+        header: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(sendData),
+        mode: 'cors'
+    };
+
+    let URL = "https://api-inference.huggingface.co/models/uer/roberta-base-chinese-extractive-qa"
+
+    fetch(URL, requestOptions)
+        .then(response => response.json())
+        .then((data) => {
+            let end = new Date();
+            show_text = `<font color="red">預測結果：</font>${data.answer}<br><font color="grey">耗時：${(end-start)/1000}秒</font>`
+            init(false)
+            Swal.fire({
+                icon: 'success',
+                title: '預測結果',
+                html: show_text,
+                showCancelButton: false,
+                confirmButtonText: '確定',
+            })
+        })
+        .catch((error) => {
+            init(false)
+            Swal.fire({
+                icon: 'error',
+                title: '超過使用次數，請稍後再試！',
+                showCancelButton: false,
+                confirmButtonText: '確定',
+            })
+        })
+}
+
+function init(fastModel) {
+    if (fastModel) {
+        $("#predictBtn").prop('disabled', false);
+        $("#predictBtn").css("display", "inline");
+        $("#loading_anim").css("display", "none");
+    } else {
+        $("#predictBtn2").prop('disabled', false);
+        $("#predictBtn2").css("display", "inline");
+        $("#loading_anim2").css("display", "none");
+    }
 }
